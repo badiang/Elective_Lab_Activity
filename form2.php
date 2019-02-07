c37f2b3beed22927c7650bdf835cd9fbb37b514a
badiang <vitloggan@gmail.com>
2019-02-06 22:21:17 -0800

    Lab Activity

---
+++ form2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>Activity</title>
+</head>
+<body>
+	<?php
+	$Yourname = $_POST ["Yourname"];
+	$Yourage = $_POST ["Yourage"];
+	echo "Hi".$Yourname.","$Yourage;
+	?>
+</body>
+</html>
